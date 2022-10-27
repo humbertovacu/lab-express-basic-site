a5c23d7563c47ab4feb1d8fe6047ff6ea4ca2e9c
humbertovacu <humbertovacu@gmail.com>
2022-10-27 21:34:55 +0200

    added views and public directories

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get('/', (request, response) => response.send("This is working"));
 
 app.listen(port, () => console.log(`My first app listening on port: ${port}`))
